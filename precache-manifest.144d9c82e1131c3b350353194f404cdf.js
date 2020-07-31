04b22f89c3c0f81351d9d3c18a599f119fcefe06
Hanwool <wjsgksdnf11@naver.com>
2020-07-31 10:27:18 +0900

    Updates

---
+++ precache-manifest.144d9c82e1131c3b350353194f404cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c30fe31ea6d46f39dac1b9c5fe16894",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fd8a401797c3eb6769fd",
+    "url": "/movie_app/static/css/main.3bb54ac9.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8a401797c3eb6769fd",
+    "url": "/movie_app/static/js/main.a8c08193.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
